c82174176bc4522994b9ab7e5fd350c85d8dae71
1543-STACK <141954178+1543-STACK@users.noreply.github.com>
2024-02-04 14:56:07 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -2,5 +2,28 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <hi>Weather App</hi>
+
+      <footer>
+        This project is coded by{" "}
+        <a
+          href="https://github.com/1543-STACK/my-weather-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Anu Govender
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/1543-STACK/my-weather-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
